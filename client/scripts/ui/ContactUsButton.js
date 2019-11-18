df2b7aab6600dd1f2ff76be7af4e76a0e678afab
Panagiotis Plytas <panagiotis.plytas@gmail.com>
2019-11-18 20:55:48 +0200

    Add ContactUsButton component in PageComponent

---
+++ client/scripts/ui/ContactUsButton.js
@@ -0,0 +1,11 @@
+
+function ContactUsButton(bind,args) {
+
+
+}
+
+ContactUsButton.prototype.displayAlert = function() {
+    alert("Clicked!");
+}
+
+module.exports = ContactUsButton;
--- client/scripts/ui/PageComponent.js
+++ client/scripts/ui/PageComponent.js
@@ -3,8 +3,9 @@ function PageComponent(bind,args) {
 
   bind(this);
 
-  fs.mix(this,'ui/SideNav');
-  fs.mix(this,'ui/MainView');
+  fs.mix(this, 'ui/SideNav');
+  fs.mix(this, 'ui/MainView');
+  fs.mix(this, 'ui/ContactUsButton');
 
 }
 
